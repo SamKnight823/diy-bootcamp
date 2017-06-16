7c7927406fcf293b71584b58a424298fad87edbf
Samantha Knight <sammay82@yahoo.com>
2017-06-16 19:32:26 +0000

    change tables

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+configure do
+     enable :sessions
+     set :session_secret, "bootcamp_secret"
+     set :public_folder, 'public'
+     set :views, 'app/views'
+    end
+
+    get '/' do
+    if logged_in?
+      get '/drills'
+    else
+      erb :index
+    end
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
